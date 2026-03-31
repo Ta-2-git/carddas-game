fcaba446396ecc435cc5bb50d969556595f82684
Ta-2-git <wtatsu4160@gmail.com>
2026-04-01 03:08:03 +0900

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <div style={{color:"white",background:"black",minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center",fontSize:24}}>ゲーム読み込み中...</div>
+}
